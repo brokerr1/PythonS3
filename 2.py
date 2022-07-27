9a009579b9f20aef8819388bbc099d9c8f773c40
brokerr1 <solovievdima1@gmail.com>
2022-07-27 17:08:12 +0300

    2

--- 2.py
+++ 2.py
@@ -3,3 +3,13 @@
 #список: ["qwe", "asd", "zxc", "qwe", "ertqwe"], ищем: "qwe", ответ: 3
 #список: ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"], ищем: "йцу", ответ: 5
 
+value1 = 'qwe'
+value2 = 'qwe asd zxc qwe ertqwe'
+
+
+n = 3
+def get_sec_entry(val1, val2):
+    start = val2.find(val1)
+    return val2.find(val1, start + 1)
+
+print(get_sec_entry(value1, value2))
