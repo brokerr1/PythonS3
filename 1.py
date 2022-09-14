95a48fd764a36c15e80bba9933bb374eb4d3a0ea
brokerr1 <solovievdima1@gmail.com>
2022-09-14 11:40:55 +0300

    1

--- 1.py
+++ 1.py
@@ -1,31 +1,8 @@
-#Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 
 from random import randint
-import itertools
+numbers = []
+for i in range(10):
+    numbers.append(randint(0, 99))
+print('Исходный список', numbers)
 
-n = 7
-    
-mylist = ['asfasf', 'asfr', 'kjaetgrhsd', '12', 'qwq', '5']
-
-def find_number(n, lst):
-    return str(n) in lst
-
-print(find_number(n, mylist))
-
-# Нахождение числа во вложенном списке
-
-def get_list(raw, col, frst, last):
-    return [[randint(frst, last) for j in range(col)] for i in range (raw)]
-
-def find_number(n, mylist):
-    return n in list(itertools.chain(*mylist))
-
-raw = 3
-col = 3
-frst = 1
-last = 100
-mylist = get_list(raw, col, frst, last)
-
-print(mylist)
-n = int(input(('Введите число: ')))
-print(find_number(n, mylist))
